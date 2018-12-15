b18197354cf3c6289a2ecc880bdd3ed728ed200e
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-15 13:40:59 +0100

    - 3 and 4 added

--- src/main/java/employees/basic/Objectives.java
+++ src/main/java/employees/basic/Objectives.java
@@ -4,9 +4,17 @@ import java.sql.*;
 
 public class Objectives {
     public static void main(String[] args) {
-//        objective2("SELECT count(*) FROM departments", "SELECT count(*) FROM employees");
+        objective2("SELECT count(*) FROM departments", "SELECT count(*) FROM employees");
         System.out.println("----------------");
         objective3("Finance");
+        System.out.println("----------------");
+        objective3("Marketing");
+        System.out.println("----------------");
+        objective3("Customer Service");
+        System.out.println("----------------");
+        objective4("Customer Service");
+        System.out.println("----------------");
+
     }
 
     private static void objective2(String... query) {
@@ -28,12 +36,51 @@ public class Objectives {
 
     private static void objective3(String deptName) {
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM departments WHERE dept_name = ?")) {
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM departments WHERE dept_name = ?");
+             PreparedStatement stmt2 = conn.prepareStatement("SELECT first_name, last_name FROM employees e " +
+                     "INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no " +
+                     "INNER JOIN departments d ON dm.dept_no = d.dept_no " +
+                     "WHERE d.dept_no = ?")) {
+            ResultSet rs;
+
             stmt.setString(1, deptName);
-            stmt.executeUpdate();
-            ResultSet rs = stmt.executeQuery();
+            rs = stmt.executeQuery();
             rs.next();
-            System.out.println(rs.getString(1));
+            String deptNo = rs.getString(1);
+
+            System.out.println(deptName + ":");
+
+            stmt2.setString(1, deptNo);
+            rs = stmt2.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println(e.getErrorCode());
+        }
+    }
+
+    private static void objective4(String deptName) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM departments WHERE dept_name = ?");
+             PreparedStatement stmt2 = conn.prepareStatement("SELECT first_name, last_name FROM employees e " +
+                     "INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no " +
+                     "INNER JOIN departments d ON dm.dept_no = d.dept_no " +
+                     "WHERE d.dept_no = ? AND to_date = '9999-01-01'")) {
+            ResultSet rs;
+
+            stmt.setString(1, deptName);
+            rs = stmt.executeQuery();
+            rs.next();
+            String deptNo = rs.getString(1);
+
+            System.out.print(deptName + " - current manager: ");
+
+            stmt2.setString(1, deptNo);
+            rs = stmt2.executeQuery();
+            rs.next();
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
             rs.close();
         } catch (SQLException e) {
             System.out.println(e.getErrorCode());
