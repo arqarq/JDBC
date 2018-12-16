15cbec4dc8c39d18dbed5096c3b923a98f64b3ea
arqarq <43341071+arqarq@users.noreply.github.com>
2018-12-16 02:41:52 +0100

    - 8 added

--- src/main/java/employees/basic/Objectives.java
+++ src/main/java/employees/basic/Objectives.java
@@ -22,9 +22,11 @@ public class Objectives {
         System.out.println("----------------");
         objective6();
         System.out.println("----------------");
-        objective7();
+        objective7("d010", "PR");
         System.out.println("----------------");
-//        objective8();
+        objective4("PR");
+        System.out.println("----------------");
+        objective8("PR");
     }
 
     private static void waitForMili(long delay) {
@@ -104,7 +106,7 @@ public class Objectives {
             System.out.println(rs.getString(1) + " " + rs.getString(2));
             rs.close();
         } catch (SQLException e) {
-            System.out.println(e.getErrorCode());
+            System.err.println("SQL error code: " + e.getErrorCode());
             waitForMili(100);
         }
     }
@@ -132,28 +134,28 @@ public class Objectives {
              Statement stmt = conn.createStatement()
         ) {
             ResultSet rs;
-            String best = "WITH best AS\n" +
-                    "\t(SELECT s.emp_no, to_date, salary\n" +
-                    "\tFROM salaries s\n" +
-                    "    WHERE to_date = '9999-01-01' \n" +
-                    "    ORDER BY salary DESC\n" +
-                    "    LIMIT 1)\n" +
-                    "SELECT first_name, last_name\n" +
-                    "FROM employees\n" +
-                    "WHERE emp_no IN (\n" +
-                    "\tSELECT emp_no \n" +
-                    "    FROM best)";
-            String worst = "WITH worst AS\n" +
-                    "\t(SELECT *\n" +
-                    "\tFROM salaries\n" +
-                    "    WHERE to_date = '9999-01-01' \n" +
-                    "    ORDER BY salary ASC\n" +
-                    "    LIMIT 1)\n" +
-                    "SELECT first_name, last_name\n" +
-                    "FROM employees\n" +
-                    "WHERE emp_no IN (\n" +
-                    "\tSELECT emp_no \n" +
-                    "    FROM worst)";
+            String best = "WITH best AS " +
+                    "(SELECT * " +
+                    "FROM salaries " +
+                    "WHERE to_date = '9999-01-01' " +
+                    "ORDER BY salary DESC " +
+                    "LIMIT 1) " +
+                    "SELECT first_name, last_name " +
+                    "FROM employees " +
+                    "WHERE emp_no IN " +
+                    "(SELECT emp_no " +
+                    "FROM best)";
+            String worst = "WITH worst AS " +
+                    "(SELECT * " +
+                    "FROM salaries " +
+                    "WHERE to_date = '9999-01-01' " +
+                    "ORDER BY salary ASC " +
+                    "LIMIT 1) " +
+                    "SELECT first_name, last_name " +
+                    "FROM employees " +
+                    "WHERE emp_no IN " +
+                    "(SELECT emp_no " +
+                    "FROM worst)";
 
             rs = stmt.executeQuery(best);
             rs.next();
@@ -170,7 +172,10 @@ public class Objectives {
         }
     }
 
-    private static void objective7() {
+    private static void objective7(String deptNo, String deptName) {
+        int empNo = 1000002;
+        int empNoManager = empNo + 1;
+
         try (Connection conn = ConnectionFactory.getConnection();
              PreparedStatement addDept = conn.prepareStatement("INSERT INTO " +
                      "departments " +
@@ -190,20 +195,20 @@ public class Objectives {
         ) {
             conn.setAutoCommit(false);
 
-            addDept.setString(1, "d010");
-            addDept.setString(2, "PR");
+            addDept.setString(1, deptNo);
+            addDept.setString(2, deptName);
             addDept.executeUpdate();
 
             addEmployee.setString(6, "2018-12-15");
             // first
-            addEmployee.setInt(1, 1000000);
+            addEmployee.setInt(1, empNo);
             addEmployee.setString(2, "1990-01-01");
             addEmployee.setString(3, "Arek");
             addEmployee.setString(4, "Sekuła");
             addEmployee.setString(5, "M");
             addEmployee.executeUpdate();
             // second
-            addEmployee.setInt(1, 1000001);
+            addEmployee.setInt(1, empNoManager);
             addEmployee.setString(2, "1991-12-12");
             addEmployee.setString(3, "Andrzej");
             addEmployee.setString(4, "Dupa");
@@ -211,14 +216,14 @@ public class Objectives {
             addEmployee.executeUpdate();
 
             // zwykły
-            addEmployeeTable.setInt(1, 1000000);
-            addEmployeeTable.setString(2, "d010");
+            addEmployeeTable.setInt(1, empNo);
+            addEmployeeTable.setString(2, deptNo);
             addEmployeeTable.setString(3, "2018-12-15");
             addEmployeeTable.setString(4, "9999-01-01");
             addEmployeeTable.executeUpdate();
             // manager
-            addEmployeeManager.setInt(1, 1000001);
-            addEmployeeManager.setString(2, "d010");
+            addEmployeeManager.setInt(1, empNoManager);
+            addEmployeeManager.setString(2, deptNo);
             addEmployeeManager.setString(3, "2018-12-15");
             addEmployeeManager.setString(4, "9999-01-01");
             addEmployeeManager.executeUpdate();
@@ -226,11 +231,11 @@ public class Objectives {
             addSalaries.setString(3, "2018-12-15");
             addSalaries.setString(4, "9999-01-01");
             // normalny
-            addSalaries.setInt(1, 1000000);
+            addSalaries.setInt(1, empNo);
             addSalaries.setInt(2, 2000);
             addSalaries.executeUpdate();
             // manager
-            addSalaries.setInt(1, 1000001);
+            addSalaries.setInt(1, empNoManager);
             addSalaries.setInt(2, 6000);
             addSalaries.executeUpdate();
 
@@ -244,25 +249,94 @@ public class Objectives {
         }
     }
 
-    private static void objective8() {
-        final String deleteEmployeesStmt = "DELETE FROM employees ee " +
-                "WHERE ee.emp_no IN (SELECT e.emp_no FROM " +
-                "employees e INNER JOIN dept_emp de " +
-                "ON e.emp_no = dm.emp_no INNER JOIN departments d " +
-                "ON d.dept_no = dm.dept_no WHERE dept_name = ?)";
+    private static void objective8(String deptToDel) {
+//        final String deleteEmployeesStmt = "DELETE FROM employees ee " +
+//                "WHERE ee.emp_no IN (SELECT e.emp_no FROM " +
+//                "employees e INNER JOIN dept_emp de " +
+//                "ON e.emp_no = dm.emp_no INNER JOIN departments d " +
+//                "ON d.dept_no = dm.dept_no WHERE dept_name = ?)";
+        final String deleteSalaries = "WITH todelete AS\n" +
+                "    (SELECT e.emp_no\n" +
+                "    FROM employees e\n" +
+                "\tINNER JOIN dept_manager dm ON e.emp_no = dm.emp_no\n" +
+                "\tINNER JOIN departments d ON dm.dept_no = d.dept_no\n" +
+                "    WHERE dept_name = ?\n" +
+                "    UNION ALL\n" +
+                "\tSELECT e.emp_no\n" +
+                "    FROM employees e\n" +
+                "    INNER JOIN dept_emp de ON e.emp_no = de.emp_no\n" +
+                "\tINNER JOIN departments d ON de.dept_no = d.dept_no\n" +
+                "    WHERE dept_name = ?)\n" +
+                "DELETE FROM salaries\n" +
+                "WHERE emp_no IN\n" +
+                "\t(SELECT emp_no\n" +
+                "    FROM todelete)";
+        final String deleteEmployeesStmt = "WITH todelete AS\n" +
+                "    (SELECT e.emp_no\n" +
+                "    FROM employees e\n" +
+                "\tINNER JOIN dept_manager dm ON e.emp_no = dm.emp_no\n" +
+                "\tINNER JOIN departments d ON dm.dept_no = d.dept_no\n" +
+                "    WHERE dept_name = ?\n" +
+                "    UNION ALL\n" +
+                "\tSELECT e.emp_no\n" +
+                "    FROM employees e\n" +
+                "    INNER JOIN dept_emp de ON e.emp_no = de.emp_no\n" +
+                "\tINNER JOIN departments d ON de.dept_no = d.dept_no\n" +
+                "    WHERE dept_name = ?)\n" +
+                "DELETE FROM employees\n" +
+                "WHERE emp_no IN\n" +
+                "\t(SELECT emp_no\n" +
+                "    FROM todelete)";
+        final String deleteDeptEmp = "WITH todelete AS\n" +
+                "\t(SELECT de.dept_no\n" +
+                "\tFROM dept_emp de\n" +
+                "    INNER JOIN departments d ON d.dept_no = de.dept_no\n" +
+                "    WHERE dept_name = ?)\n" +
+                "DELETE FROM dept_emp\n" +
+                "WHERE dept_no IN\n" +
+                "\t(SELECT dept_no\n" +
+                "    FROM todelete)";
+        final String deleteDeptManager = "WITH todelete AS\n" +
+                "\t(SELECT dm.dept_no\n" +
+                "\tFROM dept_manager dm\n" +
+                "    INNER JOIN departments d ON d.dept_no = dm.dept_no\n" +
+                "    WHERE dept_name = ?)\n" +
+                "DELETE FROM dept_manager\n" +
+                "WHERE dept_no IN\n" +
+                "\t(SELECT dept_no\n" +
+                "    FROM todelete)";
         final String deleteDepartment = "DELETE FROM departments " +
                 "WHERE dept_name = ?";
+
         try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement psSal = conn.prepareStatement(deleteSalaries);
              PreparedStatement psEmp = conn.prepareStatement(deleteEmployeesStmt);
+             PreparedStatement psDelDeptEmp = conn.prepareStatement(deleteDeptEmp);
+             PreparedStatement psDelDeptManager = conn.prepareStatement(deleteDeptManager);
              PreparedStatement psDept = conn.prepareStatement(deleteDepartment)
         ) {
-            psEmp.setString(1, "PR");
+            conn.setAutoCommit(false);
+
+            psSal.setString(1, deptToDel);
+            psSal.setString(2, deptToDel);
+            psSal.executeUpdate();
+            psEmp.setString(1, deptToDel);
+            psEmp.setString(2, deptToDel);
             psEmp.executeUpdate();
-            psDept.setString(1, "PR");
+            psDelDeptEmp.setString(1, deptToDel);
+            psDelDeptEmp.executeUpdate();
+            psDelDeptManager.setString(1, deptToDel);
+            psDelDeptManager.executeUpdate();
+            psDept.setString(1, deptToDel);
             psDept.executeUpdate();
+
+//            conn.rollback();
+            conn.commit();
+            System.out.println("Deleting " + deptToDel + " department done.");
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println(e.getErrorCode());
+//            e.printStackTrace();
+            System.err.println("SQL error code: " + e.getErrorCode());
+            waitForMili(100);
         }
     }
 }
